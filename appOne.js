4984d0e9ed5b9ce4514cfbbbfd6e71eba80eef1f
Denise F <102762395+dflowers9315@users.noreply.github.com>
2022-05-03 23:41:40 -0400

    Add files via upload

---
+++ appOne.js
@@ -0,0 +1,33 @@
+// Our Ship
+let USSAssembly = {
+
+        dodge: function(){
+            // allows us to retreat
+        }
+    }
+    // -------------------------------------------------
+    
+
+
+    const battle = () =>{
+        // attack()
+            // if(alien.life = 0){then they attack}
+
+
+        // PlayerOne
+
+
+        // PlayerTwo
+    }
+    
+    
+
+
+
+    let alienMob = []
+
+
+    // ROund => each Player gets to attack
+
+    // -----------------------------------------------------
+
